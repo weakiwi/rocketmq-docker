0cc05466f1543d66a8acdf06e6dc158527599c8c
weakiwi <670932228@qq.com>
2017-11-02 15:38:10 +0800

    add logformat support

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+FROM maven
+
+# Rocketmq version
+ENV ROCKETMQ_VERSION rocketmq-all-4.1.0-incubating
+
+# Rocketmq home
+ENV ROCKETMQ_HOME  /opt/rocketmq-${ROCKETMQ_VERSION}/distribution
+
+
+
+COPY entrypoint.sh /root/entrypoint.sh
+
+WORKDIR  ${ROCKETMQ_HOME}
+
+RUN mkdir -p \
+		/opt/logs \
+	    /opt/store
+
+ADD rocketmq-all-4.1.0-incubating.tar /opt
+
+RUN chmod -R 777 bin
+RUN chmod +x /root/entrypoint.sh
+WORKDIR /opt/rocketmq-${ROCKETMQ_VERSION}
+
+
+RUN mvn -Prelease-all -DskipTests clean install -U
+WORKDIR  ${ROCKETMQ_HOME}/distribution/target/apache-rocketmq
+ENV ROCKETMQ_HOME  /opt/rocketmq-${ROCKETMQ_VERSION}/distribution/target/apache-rocketmq
+
+ENV JAVA_OPT " -Duser.home=/opt"
+ENTRYPOINT ["/root/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,27 @@
+#!/target/apache-rocketmq/bin/bash
+
+set -x
+
+if [ "${ROCKETMQ_ROLE}x" == "broker-a"x ];
+then
+    cd ${ROCKETMQ_HOME}/target/apache-rocketmq/bin
+    bash mqbroker -c ../conf/2m-2s-sync/broker-a.properties -n rockermqnamesrv1:9876, rocketmqnamesrv2:9876
+elif [ "${ROCKETMQ_ROLE}x" == "broker-a-slave"x ];
+then
+    cd ${ROCKETMQ_HOME}/target/apache-rocketmq/bin
+    bash mqbroker -c ../conf/2m-2s-sync/broker-a-s.properties -n rockermqnamesrv1:9876, rocketmqnamesrv2:9876
+elif [ "${ROCKETMQ_ROLE}x" == "broker-b"x ];
+then
+    cd ${ROCKETMQ_HOME}/target/apache-rocketmq/bin
+    bash mqbroker -c ../conf/2m-2s-sync/broker-b.properties -n rockermqnamesrv1:9876, rocketmqnamesrv2:9876
+elif [ "${ROCKETMQ_ROLE}x" == "broker-b-slave"x ];
+then
+    cd ${ROCKETMQ_HOME}/target/apache-rocketmq/bin
+    bash mqbroker -c ../conf/2m-2s-sync/broker-b-s.properties -n rockermqnamesrv1:9876, rocketmqnamesrv2:9876
+elif [ "${ROCKETMQ_ROLE}x" == "namesrv"x ];
+then
+    cd ${ROCKETMQ_HOME}/target/apache-rocketmq/bin
+    bash mqnamesrv
+else
+    echo "env ROCKETMQ_ROLE is empty, exit......."
+fi
