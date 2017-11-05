ed011ba884d491f3d02c4e918950a1231d6141fa
weakiwi <670932228@qq.com>
2017-11-05 22:32:00 +0800

    modify entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,23 +4,18 @@ set -x
 
 if [ "${ROCKETMQ_ROLE}x" == "broker-a"x ];
 then
-    cd ${ROCKETMQ_HOME}/target/apache-rocketmq/bin
-    bash mqbroker -c ../conf/2m-2s-sync/broker-a.properties -n rockermqnamesrv1:9876, rocketmqnamesrv2:9876
+    cd ${ROCKETMQ_HOME} && sh bin/mqbroker -c ../conf/2m-2s-sync/broker-a.properties -n rockermqnamesrv1:9876, rocketmqnamesrv2:9876
 elif [ "${ROCKETMQ_ROLE}x" == "broker-a-slave"x ];
 then
-    cd ${ROCKETMQ_HOME}/target/apache-rocketmq/bin
-    bash mqbroker -c ../conf/2m-2s-sync/broker-a-s.properties -n rockermqnamesrv1:9876, rocketmqnamesrv2:9876
+    cd ${ROCKETMQ_HOME} && sh bin/mqbroker -c ../conf/2m-2s-sync/broker-a-s.properties -n rockermqnamesrv1:9876, rocketmqnamesrv2:9876
 elif [ "${ROCKETMQ_ROLE}x" == "broker-b"x ];
 then
-    cd ${ROCKETMQ_HOME}/target/apache-rocketmq/bin
-    bash mqbroker -c ../conf/2m-2s-sync/broker-b.properties -n rockermqnamesrv1:9876, rocketmqnamesrv2:9876
+    cd ${ROCKETMQ_HOME} && sh bin/mqbroker -c ../conf/2m-2s-sync/broker-b.properties -n rockermqnamesrv1:9876, rocketmqnamesrv2:9876
 elif [ "${ROCKETMQ_ROLE}x" == "broker-b-slave"x ];
 then
-    cd ${ROCKETMQ_HOME}/target/apache-rocketmq/bin
-    bash mqbroker -c ../conf/2m-2s-sync/broker-b-s.properties -n rockermqnamesrv1:9876, rocketmqnamesrv2:9876
+    cd ${ROCKETMQ_HOME} && sh bin/mqbroker -c ../conf/2m-2s-sync/broker-b-s.properties -n rockermqnamesrv1:9876, rocketmqnamesrv2:9876
 elif [ "${ROCKETMQ_ROLE}x" == "namesrv"x ];
 then
-    #ROCKETMQ_HOME=""
     cd ${ROCKETMQ_HOME} && sh bin/runserver.sh org.apache.rocketmq.namesrv.NamesrvStartup
 else
     echo "env ROCKETMQ_ROLE is empty, exit......."
