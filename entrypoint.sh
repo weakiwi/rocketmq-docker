f60535d40bc11731b6fafdcfe3ab794621a11ae3
weakiwi <670932228@qq.com>
2017-11-05 20:28:39 +0800

    modify entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,9 +20,8 @@ then
     bash mqbroker -c ../conf/2m-2s-sync/broker-b-s.properties -n rockermqnamesrv1:9876, rocketmqnamesrv2:9876
 elif [ "${ROCKETMQ_ROLE}x" == "namesrv"x ];
 then
-    cd ${ROCKETMQ_HOME}
     ROCKETMQ_HOME=""
-    sh bin/runserver.sh org.apache.rocketmq.namesrv.NamesrvStartup
+    cd ${ROCKETMQ_HOME} && sh bin/runserver.sh org.apache.rocketmq.namesrv.NamesrvStartup
 else
     echo "env ROCKETMQ_ROLE is empty, exit......."
 fi
